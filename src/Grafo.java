7f5cd4a73073c494837a6ff44246280265a33fda
João Vitor <jvcostaandrade7399@gmail.com>
2019-08-26 16:14:40 -0300

    add documentacao grafo

--- src/Grafo.java
+++ src/Grafo.java
@@ -4,22 +4,32 @@ import java.util.*;
 
 public class Grafo {
 
-    public int connectedComponents;
+    private int connectedComponents;
     private Map<Integer, GrafoNo> grafo;
 
 
     public Grafo(){
-
         connectedComponents = 0;
         grafo = new HashMap<Integer, GrafoNo>();
-
     }
 
+    /**
+     * Cria e adiciona o nó ao MapaHash, sendo a chave valor inteiro do nó.
+     * @param node valor inteiro do nó a ser criado e adicionado.
+     */
     public void adicionaNo(int node){
         grafo.put( node, new GrafoNo(node));
         connectedComponents += 1;
     }
 
+    /**
+     * Cria uma relação ente dois nós, verificando se causa um ciclo.
+     * Retorna True caso a realação for adiciona com sucesso.
+     * Retorna False caso a relação gera ciclo, removendo-a.
+     * @param node1
+     * @param node2
+     * @return
+     */
     public boolean relacionaNo(int node1, int node2){
         GrafoNo nodeA = grafo.get(node1);
         GrafoNo nodeB = grafo.get(node2);
@@ -34,8 +44,12 @@ public class Grafo {
 
     }
 
-
-    public boolean possuiCiclo(GrafoNo node){
+    /**
+     * Verifica se há ciclo a partir de um determinado nó.
+     * @param node Nó de partida.
+     * @return true caso houver clico, caso contrário não.;
+     */
+    private boolean possuiCiclo(GrafoNo node){
 
         ArrayList<GrafoNo> links = node.getLinks();
         if(links.size() == 1) return false;
@@ -50,7 +64,14 @@ public class Grafo {
         return contains;
     }
 
-    public boolean buscaNo(GrafoNo node, GrafoNo nodeKey){
+    /**
+     * Realiza uma busca por um determinado nó dentro de listas de nós,
+     * até que não haja mais elementos nestas listas ou o nó for encontrado.
+     * @param node Nó de partida.
+     * @param nodeKey Nó a ser buscado.
+     * @return true caso nó encontrado, caso contrário false.
+     */
+    private boolean buscaNo(GrafoNo node, GrafoNo nodeKey){
 
 
         if(node.getLinks().size() == 1) return false;
